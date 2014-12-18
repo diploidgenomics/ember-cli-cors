3202108aa48e4fe1e39e768b6bd3aae6dedd9d5a
Dennis Lorson <dennis@orbicule.com>
2014-12-18 15:27:39 +0100

    First version

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-/* jshint node: true */
-'use strict';
-
 module.exports = {
-  name: 'ember-cli-cors'
+  name: 'ember-cli-cors',
+
+  config: function(environment /*, appConfig */) {
+    return {};
+  },
+
+  serverMiddleware: function(config) {
+    config.app.use(function(req, res, next) {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      next();
+    });
+  }
 };
